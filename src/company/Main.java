b09d83d265a4c46e124bfd528fc5e87383710e9d
kharisisriyant <kharisisriyant@gmail.com>
2018-02-20 19:56:26 +0700

    test populate regCustomer

--- src/company/Main.java
+++ src/company/Main.java
@@ -1,5 +1,8 @@
 package company;
 
+import java.util.Date;
+import java.util.Random;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -17,17 +20,24 @@ public class Main {
         em.getTransaction().begin();
         for (int i = 0; i < 10; i++) {
             RegularCustomer r = new RegularCustomer();
+            r.name = "Orang " + i;
+            r.birth_date = generateRandomDate();
+            r.location.address = "Jalan " + i + " nomor " + i;
+            r.location.city = "Bandung";
+            r.location.post_code = "4051" + (i%10);
+            r.location.province = "Jawa Barat";
+            
             em.persist(r);
         }
         em.getTransaction().commit();
 
         // Find the number of Point objects in the database:
-        Query q1 = em.createQuery("SELECT COUNT(p) FROM Point p");
-        System.out.println("Total Points: " + q1.getSingleResult());
+        Query q1 = em.createQuery("SELECT * FROM RegularCustomer r");
+        System.out.println("Select *: " + q1.getSingleResult());
 
         // Find the average X value:
-        Query q2 = em.createQuery("SELECT AVG(p.x) FROM Point p");
-        System.out.println("Average X: " + q2.getSingleResult());
+//        Query q2 = em.createQuery("SELECT AVG(p.x) FROM Point p");
+//        System.out.println("Average X: " + q2.getSingleResult());
 
         // Retrieve all the Point objects from the database:
 //        TypedQuery<Point> query =
@@ -40,5 +50,23 @@ public class Main {
         // Close the database connection:
         em.close();
         emf.close();
+    }
+    
+    static Date generateRandomDate() {
+        Random  rnd;
+        Date    dt;
+        long    ms;
+
+        // Get a new random instance, seeded from the clock
+        rnd = new Random();
+
+        // Get an Epoch value roughly between 1940 and 2010
+        // -946771200000L = January 1, 1940
+        // Add up to 70 years to it (using modulus on the next long)
+        ms = -946771200000L + (Math.abs(rnd.nextLong()) % (70L * 365 * 24 * 60 * 60 * 1000));
+
+        // Construct a date
+        dt = new Date(ms);
+        return dt;
     }
 }
